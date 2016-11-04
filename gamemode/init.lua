2ed38e3d656c99eaa1a712bb20c36db05f917d00
Unknown <noah.stride@selfsown.com>
2016-11-04 19:17:20 +0000

    shrexoor

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -8,7 +8,7 @@ resource.AddWorkshop( "314261589" ) -- Shrek Model
 resource.AddWorkshop( "104910430" ) -- Rape Swep
 
 function GM:PlayerInitialSpawn( ply )
-	ply:SetTeam(2) --Set Team to Spectators
+	ply:SetTeam(0) --Set Team to Players
 end
 
 function GM:PlayerDeath( ply )
@@ -16,8 +16,8 @@ function GM:PlayerDeath( ply )
 end
 
 function GM:PlayerSpawn( ply )
-	if ply:Team() == 2 then
-		ply:Spectate(5)
+	if ply:Team() == 2 then --If spectator
+		ply:Spectate(5) --Make them spectate
 	end
 end
 
@@ -26,6 +26,7 @@ function team0( ply ) -- Creating the function.
 	ply:SetTeam( 0 ) -- We'll set him to team 0
 	ply:Spawn() -- Let's spawn him.
 	ply:SetModel("models/player/group01/male_07.mdl") -- Setting the Hider's PM
+	ply:SetPlayerColor( Vector(0.22, 0.5, 0.10) )
 
 end -- End the function
 concommand.Add("runner", team0) -- Adding a concommand (Console Command) for the team.
@@ -36,10 +37,52 @@ function team1( ply ) -- Creating the function.
 	ply:SetTeam( 1 ) -- We'll set him to team 1
 	ply:Spawn() -- Let's spawn him.
 	ply:SetModel("models/player/pyroteknik/shrek.mdl") -- Setting Shrek's PM
-	ply:SetPlayerColor( Vector(57, 133, 27) )
+	ply:SetPlayerColor( Vector(0.22, 0.5, 0.10) )
 	ply:Freeze (true)	-- Freeze the player
 	timer.Simple( 2, function() ply:Freeze(false) end )	-- Unfreeze the player after 60 seconds
 	ply:Give ("weapon_rape") -- Equip the player with a hider's gun
 
 end -- End the function
 concommand.Add("shrek", team1) -- Adding a concommand (Console Command) for the team.
+
+-- Round System
+Round = {}
+Round.DefaultTime = 10
+Round.CurrentTime = 0
+Round.ShrekCount = 1
+
+SetGlobalInt("TimeLeft", Round.CurrentTime)
+
+function Round.Handle() --This function runs every second
+	Round.CurrentTime = Round.CurrentTime - 1
+	SetGlobalInt("TimeLeft", Round.CurrentTime)
+
+	if Round.CurrentTime <= 0 then --If its ended
+		Round.Start() --Start the next round
+	else --Else do any stuff we want to run each second during gameplay.
+		for k, v in pairs( player.GetAll() ) do
+			v:ChatPrint("Round Time Remaining: "..Round.CurrentTime)
+		end
+	end
+
+end
+
+function Round.Start() --This runs at the start of each round
+	Round.CurrentTime = Round.DefaultTime
+	SetGlobalInt("TimeLeft", Round.CurrentTime)
+
+	local Shrek = table.Random(player.GetAll()) --Pick shrek
+	team1(Shrek) --Make random player shrek
+
+	for k, v in pairs( player.GetAll() ) do
+		v:ChatPrint("The new shrek is: "..Shrek.Nick())
+	end
+
+	for k, v in pairs( player.GetAll() ) do
+		if v != Shrek then
+			team0(v) --Spawn them as a runner
+		end
+	end
+end
+
+timer.Create("Round.Handle", 1, 0, Round.Handle)
