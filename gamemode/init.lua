81743288c7de9140cf27d10e2a03da16850eb1f2
Unknown <noah.stride@selfsown.com>
2016-11-04 17:37:30 +0000

    serious fix

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,13 +2,11 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
+include( "player.lua" )
+
+resource.AddWorkshop( "248643568" ) -- Shrek Model
 
 function GM:PlayerInitialSpawn( ply )
-	ply:SetGamemodeTeam( 2 )
-end	
-	
-function GM:PlayerSetModel(ply)
-	ply:SetModel("models/player/group01/female_07.mdl")
 end
 
 function team0( ply ) -- Creating the function.
@@ -16,11 +14,11 @@ function team0( ply ) -- Creating the function.
 	ply:SetTeam( 0 ) -- We'll set him to team 1
 	ply:Spawn() -- Let's spawn him.
 	ply:SetModel("models/player/group01/male_07.mdl") -- Setting the Hider's PM
-	
+
 end -- End the function
 concommand.Add("runner", team0) -- Adding a concommand (Console Command) for the team.
- 
- 
+
+
 function team1( ply ) -- Creating the function.
 	ply:UnSpectate() -- As soon as the person joins the team, he get's Un-spectated
 	ply:SetTeam( 1 ) -- We'll set him to team 1
@@ -28,9 +26,7 @@ function team1( ply ) -- Creating the function.
 	ply:SetModel("models/narry/shrek_playermodel_v1.mdl") -- Setting Shrek's PM
 	ply:Freeze (true)										-- Freeze the player
 	timer.Simple( 60, function() ply:Freeze(false) end )	-- Unfreeze the player after 60 seconds
-	ply:Give ("has_seekgun") -- Equip the player with a hider's gun
-	
+	ply:Give ("weapon_rape") -- Equip the player with a hider's gun
+
 end -- End the function
 concommand.Add("shrek", team1) -- Adding a concommand (Console Command) for the team.
-
-function roundstart(ply)
