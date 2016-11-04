f0ae29caae4de5e946848122f797ad2bf248fa12
Unknown <noah.stride@selfsown.com>
2016-11-04 18:24:11 +0000

    diff model

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,7 +4,8 @@ AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
 include( "player.lua" )
 
-resource.AddWorkshop( "248643568" ) -- Shrek Model
+resource.AddWorkshop( "314261589" ) -- Shrek Model
+resource.AddWorkshop( "104910430" ) -- Rape Swep
 
 function GM:PlayerInitialSpawn( ply )
 	ply:SetTeam(2) --Set Team to Spectators
@@ -14,6 +15,11 @@ function GM:PlayerDeath( ply )
 	ply:SetTeam(2) --Set Team to Spectators
 end
 
+function GM:PlayerSpawn( ply )
+	if ply:Team() == 2 then
+		ply:Spectate(5)
+	end
+end
 
 function team0( ply ) -- Creating the function.
 	ply:UnSpectate() -- As soon as the person joins the team, he get's Un-spectated
@@ -29,10 +35,11 @@ function team1( ply ) -- Creating the function.
 	ply:UnSpectate() -- As soon as the person joins the team, he get's Un-spectated
 	ply:SetTeam( 1 ) -- We'll set him to team 1
 	ply:Spawn() -- Let's spawn him.
-	ply:SetModel("models/narry/shrek_playermodel_v1.mdl") -- Setting Shrek's PM
+	ply:SetModel("models/player/pyroteknik/shrek.mdl") -- Setting Shrek's PM
+	ply:SetPlayerColor( Color(57, 133, 27) )
 	ply:Freeze (true)	-- Freeze the player
 	timer.Simple( 2, function() ply:Freeze(false) end )	-- Unfreeze the player after 60 seconds
-	ply:Give ("weapon_rapeshrek") -- Equip the player with a hider's gun
+	ply:Give ("weapon_rape") -- Equip the player with a hider's gun
 
 end -- End the function
 concommand.Add("shrek", team1) -- Adding a concommand (Console Command) for the team.
